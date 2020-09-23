4eb3e42adda76d05df3bf964d2fdf9d897288881
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-23 17:13:29 +0000

    added on connection events: console log message and sending name to the server

--- client.js
+++ client.js
@@ -8,6 +8,13 @@ const connect = () => {
   // interpret data as text
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: RG') // sending name to the game server (three character limit)
+  })
+
+
+
   conn.on('data', (data) => {
     console.log(data);
   })
